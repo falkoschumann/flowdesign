7ba51f2dd553c192b2cfc94483714e7cfceda908
Falko Schumann <falko.schumann@muspellheim.de>
2015-06-18 20:09:02 +0200

    Update documentation

--- src/main/java/de/muspellheim/flowdesign/package-info.java
+++ src/main/java/de/muspellheim/flowdesign/package-info.java
@@ -12,9 +12,12 @@
  * <ol>
  * <li><em>Build:</em> Erzeuge alle Functional-Units.</li>
  * <li><em>Bind:</em> Verbinde die Functional-Units miteinander.</li>
- * <li><em>Inject:</em> Injiziere explizite Abhängigkeiten in die Functional-Units, die {@link de.muspellheim.flowdesign.DependsOn} implentieren.</li>
- * <li><em>Configure:</em> Konfiguriere die Functional-Units, die {@link de.muspellheim.flowdesign.Configurable} implementieren.</li>
- * <li><em>Run:</em> Starte den Fluss durch den Aufruf von {@link de.muspellheim.flowdesign.EntryPoint#run(java.lang.String[])} am Startpunkt.</li>
+ * <li><em>Inject:</em> Injiziere explizite Abhängigkeiten in die Functional-Units, durch Aufruf der mit
+ * {@link javax.inject.Inject} markierten Methoden.</li>
+ * <li><em>Configure:</em> Konfiguriere die Functional-Units, durch Aufruf der mit
+ * {@link de.muspellheim.flowdesign.Configure} markierten Methoden.</li>
+ * <li><em>Run:</em> Starte den Fluss durch den Aufruf von
+ * {@link de.muspellheim.flowdesign.EntryPoint#run(java.lang.String[])} am Startpunkt.</li>
  * </ol>
  */
 package de.muspellheim.flowdesign;
